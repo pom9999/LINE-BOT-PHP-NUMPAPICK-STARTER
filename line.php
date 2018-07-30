d026068e8f7b8a2541e7a2a5b1af9f3b9e8bd150
pom9999 <41367851+pom9999@users.noreply.github.com>
2018-07-30 17:28:40 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'watcharin.k-pom',
+        'to' => 'U4c04d3b6a02e9e4629230f20293cf6d8',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
